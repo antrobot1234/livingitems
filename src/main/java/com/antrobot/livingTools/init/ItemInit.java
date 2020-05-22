9500a2cd947de5ff7b3c9f59365e7fef39853da1
antrobot1234 <antrobot1234@gmail.com>
2020-05-22 14:42:00 -0500

    added a special item that glows if you hold shift using NBT.

--- src/main/java/com/antrobot/livingTools/init/ItemInit.java
+++ src/main/java/com/antrobot/livingTools/init/ItemInit.java
@@ -1,6 +1,7 @@
 package com.antrobot.livingTools.init;
 
 import com.antrobot.livingTools.LivingTools;
+import com.antrobot.livingTools.objects.items.SpecialItem;
 import com.antrobot.livingTools.utils.ModItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.PickaxeItem;
@@ -12,5 +13,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, LivingTools.MOD_ID);
     public static final RegistryObject<Item> TESTITEM = ITEMS.register("testitem", () -> new Item(new Item.Properties().group(LivingTools.TAB)));
-    public static final RegistryObject<Item> TESTTOOL = ITEMS.register("testtool",() -> new PickaxeItem(ModItemTier.EXAMPLE,4,2,new Item.Properties().group(LivingTools.TAB)));
+    public static final RegistryObject<Item> TESTTOOL = ITEMS.register("testtool",() -> new PickaxeItem(ModItemTier.EXAMPLE,1,1,new Item.Properties().group(LivingTools.TAB)));
+    public static final RegistryObject<Item> SPECIALITEM = ITEMS.register("specialitem",()->new SpecialItem(null));
 }
