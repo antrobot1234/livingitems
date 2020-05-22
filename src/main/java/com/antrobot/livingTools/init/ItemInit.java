a2e8b7ed6988d5efd39c34e33ab75c02262dddba
antrobot1234 <antrobot1234@gmail.com>
2020-05-22 11:33:21 -0500

    added testtool pickaxe

--- src/main/java/com/antrobot/livingTools/init/ItemInit.java
+++ src/main/java/com/antrobot/livingTools/init/ItemInit.java
@@ -1,8 +1,9 @@
 package com.antrobot.livingTools.init;
 
 import com.antrobot.livingTools.LivingTools;
+import com.antrobot.livingTools.utils.ModItemTier;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.PickaxeItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,4 +12,5 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, LivingTools.MOD_ID);
     public static final RegistryObject<Item> TESTITEM = ITEMS.register("testitem", () -> new Item(new Item.Properties().group(LivingTools.TAB)));
+    public static final RegistryObject<Item> TESTTOOL = ITEMS.register("testtool",() -> new PickaxeItem(ModItemTier.EXAMPLE,4,2,new Item.Properties().group(LivingTools.TAB)));
 }
